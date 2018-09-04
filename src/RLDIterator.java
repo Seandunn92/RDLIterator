35027bfd0773f12213d13f27822502f8a5cd6c33
Seandunn92 <seandunn92@gmail.com>
2018-09-04 18:28:20 -0400

    RLD Implementor handles double letters

--- src/RLDIterator.java
+++ src/RLDIterator.java
@@ -8,9 +8,12 @@ public class RLDIterator implements Iterator {
 
     private Iterator<Character> itr;
     private int currNum;
+    private char currChar;
+
 
     RLDIterator(){
         currNum=0;
+        currChar= '*';
         char[] nameArray = "1B1r1e2t1s".toCharArray();
         ArrayList<Character> nameArrayList= new ArrayList<Character>();
         for (char nameChar : nameArray){
@@ -30,7 +33,13 @@ public class RLDIterator implements Iterator {
 
     @Override
     public Character next() {
-        itr.next();
-        return itr.next();
+
+        if(currNum==0){
+            currNum = Character.getNumericValue(itr.next());
+            currChar = itr.next();
+        }
+
+        currNum--;
+        return currChar;
     }
 }
