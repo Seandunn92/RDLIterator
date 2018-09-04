51a775f3dbe623251baa7ed640c1d6f0cae49fcb
Seandunn92 <seandunn92@gmail.com>
2018-09-04 18:19:52 -0400

    Added more logic to hasNext

--- src/RLDIterator.java
+++ src/RLDIterator.java
@@ -7,8 +7,10 @@ import java.util.Iterator;
 public class RLDIterator implements Iterator {
 
     private Iterator<Character> itr;
+    private int currNum;
 
     RLDIterator(){
+        currNum=0;
         char[] nameArray = "1B1r1e2t1s".toCharArray();
         ArrayList<Character> nameArrayList= new ArrayList<Character>();
         for (char nameChar : nameArray){
@@ -20,7 +22,10 @@ public class RLDIterator implements Iterator {
 
     @Override
     public boolean hasNext() {
+
+        if(currNum==0)
         return itr.hasNext();
+        return true;
     }
 
     @Override
