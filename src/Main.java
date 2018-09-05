faddc99e93aee95c4b926d73161b8fd785d79de4
Seandunn92 <seandunn92@gmail.com>
2018-09-04 21:19:05 -0400

    Refactoring to have an iterateAndPrint Method rather than redundant while loop for RDLIterator

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
  * Created by seandunn92 on 9/4/18.
  */
 public class Main {
+    public static RDLIterator rDLIterator;
 
     public static void main (String[] args) {
 
@@ -13,16 +14,21 @@ public class Main {
         System.out.println();
         System.out.println();
 
-        RDLIterator rDLIterator = new RDLIterator();
+        rDLIterator = new RDLIterator();
+        iterateAndPrint();
 
+
+        rDLIterator = new RDLIterator("1M1a2t");
+        iterateAndPrint();
+
+
+    }
+
+    public static void iterateAndPrint(){
         while (rDLIterator.hasNext()){
             System.out.print(rDLIterator.next());
         }
         System.out.println();
-
-        rDLIterator = new RDLIterator("1M1a2t");
-        while (rDLIterator.hasNext()){
-            System.out.print(rDLIterator.next());
-        }
+        System.out.println();
     }
 }
